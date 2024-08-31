f4300080938642f7454cc39eb6f5cb3354cbd72f
unknown <vijju949140@gmail.com>
2024-08-31 14:44:41 +0530

    color pick

---
+++ index.js
@@ -0,0 +1,24 @@
+function changeBgToGrey(){
+    document.getElementById("colorPickerContainer").style.backgroundColor="#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent="#e0e0e0";
+
+}
+
+function changeBgToGreen(){
+    document.getElementById("colorPickerContainer").style.backgroundColor="#6fcf97";
+    document.getElementById("selectedColorHexCode").textContent="#6fcf97";
+
+}
+
+function changeBgToBlue(){
+    document.getElementById("colorPickerContainer").style.backgroundColor="#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent="#56ccf2";
+
+}
+
+function changeBgToPurple(){
+    document.getElementById("colorPickerContainer").style.backgroundColor="#bb6bd9";
+    document.getElementById("selectedColorHexCode").textContent="#bb6bd9";
+
+}
+
